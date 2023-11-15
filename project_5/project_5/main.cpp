ad1873ad2ffccab1c407ee94a9bd7de5c8c20148
bzhang102 <yuxiangbrandonzhang@gmail.com>
2023-11-14 20:21:55 -0800

    basic render

--- project_5/project_5/main.cpp
+++ project_5/project_5/main.cpp
@@ -10,7 +10,7 @@ bool getToken(char token[], istream& inf) {
     char c;
     while(inf.get(c)) {
         //appends to token
-        if(!isspace(c)) {
+        if(!isspace(c) && c != '\n' && c != '\t') {
             token[strlen(token)] = c;
         } else {
             //end of token reached (hit whitespace)
@@ -28,13 +28,39 @@ int render(int lineLength, istream& inf, ostream& outf) {
         return 2;
     }
     
-    //for each token in inf
+    int len = 0;
+    //iterates through input as series of tokens
     for(;;) {
         char token[181] = "";
         
         //breaks at end of file while reading next token
         if(getToken(token, inf)) {
             break;
+        }
+        
+        //checks for and executes paragraph break
+        if(strcmp(token, "@P@") == 0) {
+            outf << endl << endl;
+            len = 0;
+            
+        //if not paragraph break, attempts to render on current line
+        } else {
+            if(len + strlen(token) + 1 <= lineLength) {
+                //appends space if token is not empty and token is not first in line
+                if(len != 0 && strlen(token) != 0) {
+                    outf << " ";
+                    len++;
+                }
+                
+                outf << token;
+                len += strlen(token);
+            //word does not fit
+            } else {
+                //reset len to equal current token length
+                len = (int) strlen(token);
+                //render token on next line
+                outf << endl << token;
+            }
         }
     }
     
@@ -51,8 +77,7 @@ int main()
     if(!infile) {
         cout << "NO OUTPUT" << endl;
     }
-    int len = 10;
-    
+    int len = 40;
     return render(len, infile, outfile);
 
 }
