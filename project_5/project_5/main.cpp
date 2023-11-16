b068f2a88ca2ef3c58ac8c1f775b5d108ba5db60
bzhang102 <yuxiangbrandonzhang@gmail.com>
2023-11-16 05:10:25 -0800

    bugs fixed, but needs cleaning up

--- project_5/project_5/main.cpp
+++ project_5/project_5/main.cpp
@@ -10,7 +10,7 @@ bool getToken(char token[], istream& inf) {
     char c;
     while(inf.get(c)) {
         //appends to token and continues
-        if(!isspace(c) && c != '-') {
+        if(!isspace(c) && (c != '-')) {
             token[strlen(token)] = c;
             
             //appends to token and returns if hyphen
@@ -35,12 +35,19 @@ int render(int lineLength, istream& inf, ostream& outf) {
         return 2;
     }
     
+    //state trackers
     bool bad = false;
-    bool prependBreak = false;
     int len = 0;
+    
+    //prepend trackers
+    bool prependBreak = false;
+    int prependSpaces = 0;
     char prevToken[181] = "@P@";
+    char lastChar = '\0';
+    
     //iterates through input as series of tokens
     for(;;) {
+        //defines current token cstring within to avoid repeated wiping
         char token[181] = "";
         
         //reads next token; breaks at end of file
@@ -48,9 +55,9 @@ int render(int lineLength, istream& inf, ostream& outf) {
             break;
         }
         
-        //skips over empty tokens
+        //avoids empty tokens
         if(strlen(token) != 0) {
-            //checks for and prepares for break
+            //checks for break
             if(strcmp(token, "@P@") == 0) {
                 //avoids leading and consecutive breaks
                 if(strcmp(prevToken, "@P@") != 0) {
@@ -59,12 +66,28 @@ int render(int lineLength, istream& inf, ostream& outf) {
                 
                 //checks if token is longer than maximum line length, if so, split renders
             } else if(strlen(token) > lineLength) {
+                //prepends break
                 if(prependBreak) {
                     outf << endl << endl;
                     len = 0;
+                    prependSpaces = 0;
                     prependBreak = false;
                 }
                 
+                //prepends spaces if possible
+                if(len + prependSpaces < lineLength) {
+                    while(prependSpaces > 0) {
+                        outf << ' ';
+                        prependSpaces--;
+                        len++;
+                    }
+                } else {
+                    outf << endl;
+                    len = 0;
+                    prependSpaces = 0;
+                }
+                
+                //renders oversize token char by char
                 for(int i = 0; i < strlen(token); i++) {
                     //render char onto current line
                     if(len + 1 <= lineLength) {
@@ -77,50 +100,78 @@ int render(int lineLength, istream& inf, ostream& outf) {
                         len = 1;
                     }
                 }
+                
+                lastChar = token[strlen(token) - 1];
+                switch(lastChar) {
+                    case '-':
+                        prependSpaces = 0;
+                        break;
+                    case '.':
+                    case '?':
+                    case '!':
+                    case ':':
+                        prependSpaces = 2;
+                        break;
+                    default:
+                        prependSpaces = 1;
+                }
+                
+                //generates error state of 1
                 bad = true;
                 
                 //attempts to render on current line
             } else {
-                //gets last character
-                char lastChar = prevToken[strlen(prevToken) - 1];
-                
+                //prepends break
                 if(prependBreak) {
                     outf << endl << endl;
                     len = 0;
+                    prependSpaces = 0;
                     prependBreak = false;
                 }
                 
-                //case: first token
-                if(len == 0 && strlen(token) <= lineLength) {
+                //attempts to render string with prepended spaces
+                if(len + prependSpaces + strlen(token) <= lineLength) {
+                    //render required spaces
+                    while(prependSpaces > 0) {
+                        outf << ' ';
+                        prependSpaces--;
+                        len++;
+                    }
+                    
+                    //render token
                     outf << token;
                     len += strlen(token);
                     
-                    //case: last character was hypen
-                } else if(lastChar == '-' && len + strlen(token) <= lineLength) {
-                    outf << token;
-                    len += strlen(token);
-                    
-                    //case: last character was normal character, prepend one space
-                } else if((lastChar != '.' && lastChar != '?' && lastChar != '!' && lastChar != ':') && len +           strlen(token) + 1 <= lineLength) {
-                    outf << " " << token;
-                    len += strlen(token) + 1;
-                    
-                    //case: last character was punctuation, prepend two spaces
-                } else if((lastChar == '.' || lastChar == '?' || lastChar == '!' || lastChar == ':') &&
-                          len + strlen(token) + 2 <= lineLength) {
-                    outf << "  " << token;
-                    len += strlen(token) + 2;
-                    
-                    //case: word does not fit, start new line
+                    //string does not fit
                 } else {
-                    //reset len to equal current token length
+                    prependSpaces = 0;
                     len = (int) strlen(token);
                     //render token on next line
                     outf << endl << token;
                 }
+                
+                lastChar = token[strlen(token) - 1];
+                switch(lastChar) {
+                    case '-':
+                        prependSpaces = 0;
+                        break;
+                    case '.':
+                    case '?':
+                    case '!':
+                    case ':':
+                        prependSpaces = 2;
+                        break;
+                    default:
+                        prependSpaces = 1;
+                }
             }
             //update prevToken
             strcpy(prevToken, token);
+        } else {
+            //catches case where whitespace follows hyphen
+            if(lastChar == '-') {
+                prependSpaces = 1;
+            }
         }
     }
     
@@ -128,7 +179,6 @@ int render(int lineLength, istream& inf, ostream& outf) {
     if(bad) {
         return 1;
     }
-    //returns 0 if state normal
     return 0;
 }
 
@@ -142,6 +192,6 @@ int main()
     if(!infile) {
         cerr << "NO OUTPUT" << endl;
     }
-    int len = 40;
+    int len = 10;
     return render(len, infile, outfile);
 }
